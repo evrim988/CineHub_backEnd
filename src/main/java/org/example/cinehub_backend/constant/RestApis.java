4d6259306812005621dd4c0ae18d34c5126c3187
User <evrim98@outlook.com>
2025-01-13 16:09:36 +0300

    Initial commit

---
+++ src/main/java/org/example/cinehub_backend/constant/RestApis.java
@@ -0,0 +1,11 @@
+package org.example.cinehub_backend.constant;
+
+public class RestApis {
+
+    private static final String VERSION = "/v1";
+    private static final String DEVELOPER = "/dev";
+    private static final String ROOT = VERSION+ DEVELOPER;
+
+    public static final String USER = ROOT + "/user";
+
+}
