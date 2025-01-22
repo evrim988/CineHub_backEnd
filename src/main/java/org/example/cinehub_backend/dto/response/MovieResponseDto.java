a2012917e5e73c31d96ce8a2c9fa27c6aca71d75
User <evrim98@outlook.com>
2025-01-22 21:41:07 +0300

    movie dto oluşturuldu.

---
+++ src/main/java/org/example/cinehub_backend/dto/response/MovieResponseDto.java
@@ -0,0 +1,10 @@
+package org.example.cinehub_backend.dto.response;
+
+public record MovieResponseDto(
+        String title,
+        String description,
+        Integer year,
+        String genre,
+        String posterUrl
+) {
+}
